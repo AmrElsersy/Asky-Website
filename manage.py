36742fdbd1e20db894461c33516d851a1bafc9a9
AmrElsersy <amrelsersay@gmail.com>
2020-07-14 06:53:24 +0200

    ya rab
    Signed-off-by: AmrElsersy <amrelsersay@gmail.com>

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import *
-from models import *
+from app import app
+from models import db
 
 
 migrate = Migrate(app, db)
